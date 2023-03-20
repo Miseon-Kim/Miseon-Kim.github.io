09a0ec374c23a4548c310ff0d4fa8e6d9aaa6327
Miseon-Kim <63826154+Miseon-Kim@users.noreply.github.com>
2023-03-20 10:41:15 +0900

    Create data2.js

---
+++ shop/static/data2.js
@@ -0,0 +1,20 @@
+[
+  {
+    "id" : 3,
+    "title" : "Flowey",
+    "content" : "only 5 inches",
+    "price" : "115000"
+  },
+  {
+    "id" : 4,
+    "title" : "Baby Shoes",
+    "content" : "for less than 6",
+    "price" : "95000"
+  },
+  {
+    "id" : 5,
+    "title" : "Red Herring",
+    "content" : "Born in France",
+    "price" : "131000"
+  }
+]
