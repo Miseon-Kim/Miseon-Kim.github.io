2aceee06d232a61676e324115a98a18dcdde98a1
Miseon-Kim <63826154+Miseon-Kim@users.noreply.github.com>
2023-03-20 14:27:02 +0900

    Create data3.js

---
+++ shop/static/data3.js
@@ -0,0 +1,20 @@
+[
+  {
+    "id" : 6,
+    "title" : "Sijang",
+    "content" : "from Dongmyo station",
+    "price" : 117000
+  },
+  {
+    "id" : 7,
+    "title" : "Tag gari",
+    "content" : "ali and taobao",
+    "price" : 126000
+  },
+  {
+    "id" : 8,
+    "title" : "Flyer",
+    "content" : "Refund not available",
+    "price" : 105000
+  }
+]
